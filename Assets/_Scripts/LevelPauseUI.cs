4cb5f3cbc3aeb4b177d04629edff24ec9ff23ce8
Dani Hmaidan <danihmeidan@gmail.com>
2026-03-16 02:03:57 +0200

    Added Pause Menu Script For Levels

---
+++ Assets/_Scripts/LevelPauseUI.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelPauseUI : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu; // assign in Inspector
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void ReturnToHub()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Hub"); // hub scene name
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
